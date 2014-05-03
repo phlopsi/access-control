eba8ce703177cc9458c1bf1ed3e4fe55f8ab0be4
Patrick Fischer <nbphobos@gmail.com>
2014-05-03 11:15:05 +0200

    added more tests

--- tests/phlopsi/access_control/AccessControlTest.php
+++ tests/phlopsi/access_control/AccessControlTest.php
@@ -21,7 +21,7 @@ class AccessControlTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @covers AccessControl::createPermission
+     * @covers phlopsi\access_control\AccessControl::createPermission
      * @expectedException \InvalidArgumentException
      */
     public function testCreatePermissionInvalidArgumentException()
@@ -29,4 +29,93 @@ class AccessControlTest extends \PHPUnit_Framework_TestCase
         $this->access_control->createPermission(NULL);
     }
 
+    /**
+     * @covers phlopsi\access_control\AccessControl::createRole
+     * @expectedException \InvalidArgumentException
+     */
+    public function testCreateRoleInvalidArgumentException()
+    {
+        $this->access_control->createRole(NULL);
+    }
+
+    /**
+     * @covers phlopsi\access_control\AccessControl::createSessionType
+     * @expectedException \InvalidArgumentException
+     */
+    public function testCreateSessionTypeInvalidArgumentException()
+    {
+        $this->access_control->createSessionType(NULL);
+    }
+
+    /**
+     * @covers phlopsi\access_control\AccessControl::createUser
+     * @expectedException \InvalidArgumentException
+     */
+    public function testCreateUserInvalidArgumentException()
+    {
+        $this->access_control->createUser(NULL);
+    }
+
+    /**
+     * @covers phlopsi\access_control\AccessControl::deletePermission
+     * @expectedException \InvalidArgumentException
+     */
+    public function testDeletePermissionInvalidArgumentException()
+    {
+        $this->access_control->deletePermission(NULL);
+    }
+
+    /**
+     * @covers phlopsi\access_control\AccessControl::deleteRole
+     * @expectedException \InvalidArgumentException
+     */
+    public function testDeleteRoleInvalidArgumentException()
+    {
+        $this->access_control->deleteRole(NULL);
+    }
+
+    /**
+     * @covers phlopsi\access_control\AccessControl::deleteSessionType
+     * @expectedException \InvalidArgumentException
+     */
+    public function testDeleteSessionTypeInvalidArgumentException()
+    {
+        $this->access_control->deleteSessionType(NULL);
+    }
+
+    /**
+     * @covers phlopsi\access_control\AccessControl::deleteUser
+     * @expectedException \InvalidArgumentException
+     */
+    public function testDeleteUserInvalidArgumentException()
+    {
+        $this->access_control->deleteUser(NULL);
+    }
+
+    /**
+     * @covers phlopsi\access_control\AccessControl::retrieveRole
+     * @expectedException \InvalidArgumentException
+     */
+    public function testRetrieveRoleInvalidArgumentException()
+    {
+        $this->access_control->retrieveRole(NULL);
+    }
+
+    /**
+     * @covers phlopsi\access_control\AccessControl::retrieveSessionType
+     * @expectedException \InvalidArgumentException
+     */
+    public function testRetrieveSessionTypeInvalidArgumentException()
+    {
+        $this->access_control->retrieveSessionType(NULL);
+    }
+
+    /**
+     * @covers phlopsi\access_control\AccessControl::retrieveUser
+     * @expectedException \InvalidArgumentException
+     */
+    public function testRetrieveUserInvalidArgumentException()
+    {
+        $this->access_control->retrieveUser(NULL);
+    }
 }
