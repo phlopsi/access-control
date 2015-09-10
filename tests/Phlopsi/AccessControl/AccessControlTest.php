fd0cc4d51f241bfc7155b3025ab70d158888d21b
Patrick Fischer <nbphobos@gmail.com>
2015-09-10 21:43:15 +0200

    Remove TypeError tests

--- tests/Phlopsi/AccessControl/AccessControlTest.php
+++ tests/Phlopsi/AccessControl/AccessControlTest.php
@@ -75,15 +75,6 @@ class AccessControlTest extends \PHPUnit_Extensions_Database_TestCase
     {
         
     }
-
-    /**
-     * @covers \Phlopsi\AccessControl\AccessControl::createPermission
-     * @expectedException \TypeError
-     */
-    public function testCreatePermissionTypeError()
-    {
-        $this->access_control->createPermission(null);
-    }
     
     /**
      * @covers \Phlopsi\AccessControl\AccessControl::createPermission
@@ -102,95 +93,5 @@ class AccessControlTest extends \PHPUnit_Extensions_Database_TestCase
     {
         $this->access_control->createPermission('TEST_PERMISSION');
         $this->access_control->createPermission('TEST_PERMISSION');
-    }
-
-    /**
-     * @covers \Phlopsi\AccessControl\AccessControl::createRole
-     * @expectedException \TypeError
-     */
-    public function testCreateRoleTypeError()
-    {
-        $this->access_control->createRole(null);
-    }
-
-    /**
-     * @covers \Phlopsi\AccessControl\AccessControl::createSessionType
-     * @expectedException \TypeError
-     */
-    public function testCreateSessionTypeTypeError()
-    {
-        $this->access_control->createSessionType(null);
-    }
-
-    /**
-     * @covers \Phlopsi\AccessControl\AccessControl::createUser
-     * @expectedException \TypeError
-     */
-    public function testCreateUserTypeError()
-    {
-        $this->access_control->createUser(null);
-    }
-
-    /**
-     * @covers \Phlopsi\AccessControl\AccessControl::deletePermission
-     * @expectedException \TypeError
-     */
-    public function testDeletePermissionTypeError()
-    {
-        $this->access_control->deletePermission(null);
-    }
-
-    /**
-     * @covers \Phlopsi\AccessControl\AccessControl::deleteRole
-     * @expectedException \TypeError
-     */
-    public function testDeleteRoleTypeError()
-    {
-        $this->access_control->deleteRole(null);
-    }
-
-    /**
-     * @covers \Phlopsi\AccessControl\AccessControl::deleteSessionType
-     * @expectedException \TypeError
-     */
-    public function testDeleteSessionTypeTypeError()
-    {
-        $this->access_control->deleteSessionType(null);
-    }
-
-    /**
-     * @covers \Phlopsi\AccessControl\AccessControl::deleteUser
-     * @expectedException \TypeError
-     */
-    public function testDeleteUserTypeError()
-    {
-        $this->access_control->deleteUser(null);
-    }
-
-    /**
-     * @covers \Phlopsi\AccessControl\AccessControl::retrieveRole
-     * @expectedException \TypeError
-     */
-    public function testRetrieveRoleTypeError()
-    {
-        $this->access_control->retrieveRole(null);
-    }
-
-    /**
-     * @covers \Phlopsi\AccessControl\AccessControl::retrieveSessionType
-     * @expectedException \TypeError
-     */
-    public function testRetrieveSessionTypeTypeError()
-    {
-        $this->access_control->retrieveSessionType(null);
-    }
-
-    /**
-     * @covers \Phlopsi\AccessControl\AccessControl::retrieveUser
-     * @expectedException \TypeError
-     */
-    public function testRetrieveUserTypeError()
-    {
-        $this->access_control->retrieveUser(null);
     }
 }
