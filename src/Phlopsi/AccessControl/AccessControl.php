558b1cbb9af36a7923ac52b24943233d7995804f
Patrick Fischer <nbphobos@gmail.com>
2015-11-07 22:14:08 +0100

    Remove SessionTypes temporarily

--- src/Phlopsi/AccessControl/AccessControl.php
+++ src/Phlopsi/AccessControl/AccessControl.php
@@ -11,8 +11,6 @@ use Phlopsi\AccessControl\Propel\Permission as PropelPermission;
 use Phlopsi\AccessControl\Propel\PermissionQuery as PropelPermissionQuery;
 use Phlopsi\AccessControl\Propel\Role as PropelRole;
 use Phlopsi\AccessControl\Propel\RoleQuery as PropelRoleQuery;
-use Phlopsi\AccessControl\Propel\SessionType as PropelSessionType;
-use Phlopsi\AccessControl\Propel\SessionTypeQuery as PropelSessionTypeQuery;
 use Phlopsi\AccessControl\Propel\User as PropelUser;
 use Phlopsi\AccessControl\Propel\UserQuery as PropelUserQuery;
 use Propel\Runtime\Connection\ConnectionInterface;
@@ -77,30 +75,6 @@ class AccessControl
                 ->save($this->connection);
 
             return new Role($new_role);
-        });
-    }
-
-    /**
-     * @param string $session_type_id
-     *
-     * @return \Phlopsi\AccessControl\SessionType
-     *
-     * @throws \Phlopsi\AccessControl\Exception\LengthException
-     * @throws \Phlopsi\AccessControl\Exception\RuntimeException
-     */
-    public function createSessionType($session_type_id)
-    {
-        return $this->execute(function () use ($session_type_id) {
-            if (empty($session_type_id)) {
-                throw new LengthException(LengthException::ARGUMENT_IS_EMPTY_STRING);
-            }
-
-            $new_session_type = new PropelSessionType();
-            $new_session_type
-                ->setExternalId($session_type_id)
-                ->save($this->connection);
-
-            return new SessionType($new_session_type);
         });
     }
 
@@ -185,34 +159,6 @@ class AccessControl
     }
 
     /**
-     * @param string $session_type_id
-     *
-     * @return bool
-     *
-     * @throws \Phlopsi\AccessControl\Exception\LengthException
-     * @throws \Phlopsi\AccessControl\Exception\RuntimeException
-     */
-    public function deleteSessionType($session_type_id)
-    {
-        return $this->execute(function () use ($session_type_id) {
-            if (empty($session_type_id)) {
-                throw new LengthException(LengthException::ARGUMENT_IS_EMPTY_STRING);
-            }
-
-            $session_type = PropelSessionTypeQuery::create()
-                ->findOneByExternalId($session_type_id, $this->connection);
-
-            if (is_null($session_type)) {
-                return false;
-            }
-
-            $session_type->delete($this->connection);
-
-            return true;
-        });
-    }
-
-    /**
      * @param string $user_id
      *
      * @return bool
@@ -263,33 +209,6 @@ class AccessControl
             }
 
             return new Role($role);
-        });
-    }
-
-    /**
-     * @param string $session_type_id
-     *
-     * @return \Phlopsi\AccessControl\SessionType
-     *
-     * @throws \Phlopsi\AccessControl\Exception\LengthException
-     * @throws \Phlopsi\AccessControl\Exception\RuntimeException
-     */
-    public function retrieveSessionType($session_type_id)
-    {
-        return $this->execute(function () use ($session_type_id) {
-            if (empty($session_type_id)) {
-                throw new LengthException(LengthException::ARGUMENT_IS_EMPTY_STRING);
-            }
-
-            $session_type = PropelSessionTypeQuery::create()
-                ->findOneByExternalId($session_type_id, $this->connection);
-
-            if (is_null($session_type)) {
-                throw new RuntimeException(sprintf(RuntimeException::ENTITY_NOT_FOUND, $session_type_id));
-            }
-
-
-            return new SessionType($session_type);
         });
     }
 
